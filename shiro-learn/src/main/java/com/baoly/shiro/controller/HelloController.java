693a563e0323f53231b4e91efeb7e4c423f987f3
baoly <nightcatbaoly@163.com>
2021-12-10 00:24:11 +0800

    add exception handler

--- shiro-learn/src/main/java/com/baoly/shiro/controller/HelloController.java
+++ shiro-learn/src/main/java/com/baoly/shiro/controller/HelloController.java
@@ -1,12 +1,12 @@
 package com.baoly.shiro.controller;
 
 import com.baoly.shiro.service.HelloService;
+import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -16,10 +16,17 @@ public class HelloController {
     @Autowired
     private HelloService helloService;
 
-    @GetMapping("/hello")
+    @GetMapping(value = "/hello", produces = "text/html;charset=utf-8")
     @ResponseBody
     public String hello() {
-        return helloService.hello();
+        String hello = null;
+        try {
+            hello = helloService.hello();
+            return hello;
+        } catch (AuthorizationException e) {
+            return "权限不足";
+        }
+
     }
 
     @GetMapping(value = "/admin")
